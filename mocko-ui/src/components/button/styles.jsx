bde1b6c9f71181e4f174df30931255ea1a8cbce0
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2020-07-19 10:24:33 -0300

    mocko-ui: Adding mocks empty state

--- mocko-ui/src/components/button/styles.jsx
+++ mocko-ui/src/components/button/styles.jsx
@@ -18,7 +18,7 @@ export const ButtonView = styled.button`
 	cursor: pointer;
 	overflow: hidden;
 	color: #FFF;
-	background-color: #4CAF50;
+	background-color: #2E7D32;
 	/* box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2),
 				0 2px 2px  0   rgba(0, 0, 0, 0.14),
 				0 1px 5px  0   rgba(0, 0, 0, 0.12); */
---
+++ mocko-ui/src/containers/mock-list/styles.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const NoMocks = styled.div`
+  margin-top: 1.5rem;
+  text-align: center;
+  font-size: 1.25rem;
+  font-family: Montserrat, Roboto, Arial, sans-serif;
+  * {
+    margin-bottom: 1.5rem;
+  }
+  
+  button {
+    margin-top: 0.5rem;
+  }
+`;
