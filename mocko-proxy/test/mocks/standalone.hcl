7945e17705a3ee86f8d93d66b446da040643557e
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2021-01-13 14:19:55 -0300

    mocko-proxy: Moving some tests from e2e-tests to mocko-proxy

--- mocko-proxy/test/mocks/standalone.hcl
+++ mocko-proxy/test/mocks/standalone.hcl
@@ -2,13 +2,16 @@
 # status
 #-----------------------------
 mock "GET /default-status" { }
+mock "PUT /default-status" { }
+mock "DELETE /default-status" { }
 mock "POST /default-status" { }
 mock "GET /other-status" {
     status = 204
 }
+mock "* /wildcard-no-status" { }
 
 #-----------------------------
-# headers
+# mock definition
 #-----------------------------
 mock "GET /headers" {
     headers {
@@ -16,9 +19,27 @@ mock "GET /headers" {
     }
 }
 
-#-----------------------------
-# body
-#-----------------------------
 mock "GET /body" {
     body = "Hello from Mocko :)"
+}
+
+mock "GET /delay" {
+    delay = 200
+}
+
+#-----------------------------
+# mock definition
+#-----------------------------
+mock "GET /vhost" {
+    body = "global"
+}
+
+mock "GET /vhost" {
+    host = "mocko.dev"
+    body = "vhost"
+}
+
+mock "GET /vhost-only" {
+    host = "mocko.dev"
+    body = "vhost"
 }
--- mocko-proxy/test/standalone.js
+++ mocko-proxy/test/standalone.js
@@ -5,6 +5,7 @@ const helpersSuite = require('./helpers');
 
 const Code = require('@hapi/code');
 const Lab = require('@hapi/lab');
+const Hoek = require('@hapi/hoek');
 const Mocko = require('../dist/main');
 const Axios = require('axios');
 
@@ -31,9 +32,13 @@ describe('standalone', () => {
             expect(status).to.equal(204);
         });
 
-        it('defaults the status to 200 for GET mocks', async () => {
-            const { status } = await mocko.get('/default-status');
-            expect(status).to.equal(200);
+        it('defaults the status to 200 for GET, PUT and DELETE mocks', async () => {
+            const get = await mocko.get('/default-status');
+            expect(get.status).to.equal(200);
+            const put = await mocko.put('/default-status');
+            expect(put.status).to.equal(200);
+            const del = await mocko.delete('/default-status');
+            expect(del.status).to.equal(200);
         });
 
         it('defaults the status to 201 for POST mocks', async () => {
@@ -41,23 +46,81 @@ describe('standalone', () => {
             expect(status).to.equal(201);
         });
 
+        it('defaults the status to 200 for wildcard mocks', async () => {
+            const post = await mocko.post('/wildcard-no-status');
+            expect(post.status).to.equal(200);
+            const get = await mocko.get('/wildcard-no-status');
+            expect(get.status).to.equal(200);
+        });
+
         it('sets the status with the status parameter', async () => {
             const { status } = await mocko.get('/other-status');
             expect(status).to.equal(204);
         });
+
+        it('returns 404 on undefined routes', async () => {
+            let code;
+
+            await mocko.get('/undefined-route')
+                .then(({ status }) => code = status)
+                .catch(({ response }) => code = response.status);
+            
+            expect(code).to.equal(404);
+        });
     });
 
-    describe('headers', () => {
+    describe('mock definition', () => {
         it('should set headers with the headers param', async () => {
             const { headers } = await mocko.get('/headers');
             expect(headers).to.contain({ 'x-custom-header': 'foo' });
         });
-    });
 
-    describe('body', () => {
         it('should set the body with the body param', async () => {
             const { data } = await mocko.get('/body');
             expect(data).to.equal('Hello from Mocko :)');
+        });
+
+        it('should delay with the delay param', async () => {
+            const timer = new Hoek.Bench();
+            await mocko.get('/delay');
+            const error = Math.abs(timer.elapsed() - 200);
+
+            expect(error).to.be.below(20);
+        });
+    });
+
+    describe('vhost', () => {
+        it('registering a route with vhost should not override the global one', async () => {
+            const { data } = await mocko.get('/vhost');
+            expect(data).to.equal('global');
+        });
+    
+        it('request with vhost should have higher priority', async () => {
+            const { data } = await mocko.get('/vhost', {
+                headers: {
+                    Host: 'mocko.dev'
+                }
+            });
+            expect(data).to.equal('vhost');
+        });
+    
+        it('vhost route should not be accessible globally', async () => {
+            let code;
+
+            await mocko.get('/vhost-only')
+                .then(({ status }) => code = status)
+                .catch(({ response }) => code = response.status);
+
+            expect(code).to.equal(404);
+        });
+    
+        it('request with vhost only must work with vhost', async () => {
+            const { data } = await mocko.get('/vhost-only', {
+                headers: {
+                    Host: 'mocko.dev'
+                }
+            });
+            expect(data).to.equal('vhost');
         });
     });
 
