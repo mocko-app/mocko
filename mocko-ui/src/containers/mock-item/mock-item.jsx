da822926d36979ee514d193f82381d61d99d249a
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2021-03-14 09:41:03 -0300

    mocko-ui: Fixing glitch where the delete button opened crud card

--- mocko-ui/src/containers/mock-item/mock-item.jsx
+++ mocko-ui/src/containers/mock-item/mock-item.jsx
@@ -7,8 +7,13 @@ export function MockItem(params) {
     const { name, method, path, onRemove } = params;
     const [isOpened, setOpened] = useState(false);
 
+    const remove = e => {
+        e.stopPropagation();
+        onRemove();
+    };
+
     const deleteBtn = (
-        <Icon margin="s" onClick={ onRemove }>
+        <Icon margin="s" onClick={ remove }>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z" fill="white"/>
             </svg>
