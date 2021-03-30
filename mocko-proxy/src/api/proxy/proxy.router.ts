0428a851eeafae8e761af49fbe2fc4dca187465a
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2021-03-30 07:20:57 -0300

    mocko-proxy: Proxying host stanzas

--- mocko-proxy/src/api/proxy/proxy.router.ts
+++ mocko-proxy/src/api/proxy/proxy.router.ts
@@ -1,14 +1,19 @@
 import {IRouter} from "../../utils/router";
 import {Provider} from "../../utils/decorators/provider";
 import {ProxyController} from "./proxy.controller";
+import { ProxyService } from "./proxy.service";
+import { Host } from "../../definitions/data/host";
 
 @Provider()
 export class ProxyRouter implements IRouter {
     constructor(
         private readonly controller: ProxyController,
+        private readonly service: ProxyService,
     ) { }
 
     async getRoutes() {
+        const hosts = await this.service.getHosts();
+
         return [{
             method: '*',
             path: '/{any*}',
@@ -22,6 +27,24 @@ export class ProxyRouter implements IRouter {
             rules: {
                 mapSilently: true,
             },
-        }];
+        }, ...hosts.map(h => this.hostToRoute(h))];
+    }
+
+    hostToRoute(host: Host) {
+        return {
+            method: '*',
+            path: '/{any*}',
+            vhost: host.source,
+            handler: (req, h) => this.controller.proxyRequest(req, h, host.destination),
+            config: {
+                payload: {
+                    output: 'stream',
+                    parse: false,
+                },
+            },
+            rules: {
+                mapSilently: true,
+            },
+        };
     }
 }
